acb684db5bc9453d45637f404fae9b40252130df
Cao Thắng Lý <thangly2k1@gmail.com>
2025-05-21 04:28:22 +0700

    create: admin account

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('seed')
+  async seed() {
+    const message = await this.userService.createDemoAdminIfNotExists();
+    return { message };
+  }
+}
